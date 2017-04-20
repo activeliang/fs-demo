49c68029c9ccaaa193a65903014a5f6bca159264
activeliang <activeliang@gmail.com>
2017-04-20 20:48:18 +0800

    welcome part1

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
-
+  get 'dash' => 'welcome'
   resources :resumes do
     member do
       post :employ_it
