cc71768adf46fa401053c6e5afeadd5d7c0b2781
activeliang <activeliang@gmail.com>
2017-04-21 17:00:49 +0800

    change footer position

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,19 @@
 //= require bootstrap-sprockets
 //= require html5_shiv
 //= require_tree .
+
+
+// 以下是固定footer的js
+$(function(){
+    function footerPosition(){
+        $("footer").removeClass("fixed-bottom");
+        var contentHeight = document.body.scrollHeight,//网页正文全文高度
+            winHeight = window.innerHeight;//可视窗口高度，不包括浏览器顶部工具栏
+        if(!(contentHeight > winHeight)){
+            //当网页正文高度小于可视窗口高度时，为footer添加类fixed-bottom
+            $("footer").addClass("fixed-bottom");
+        }
+    }
+    footerPosition();
+    $(window).resize(footerPosition);
+});
