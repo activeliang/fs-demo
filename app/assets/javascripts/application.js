048314f8bdc645c2e263c2a2456b0e930d833f40
activeliang <activeliang@gmail.com>
2017-04-25 20:41:06 +0800

    no change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,8 +34,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-        document.getElementById("myBtn").style.display = "block";
+        document.getElementById("liangll").style.display = "block";
     } else {
-        document.getElementById("myBtn").style.display = "none";
+        document.getElementById("liangll").style.display = "none";
     }
 }
